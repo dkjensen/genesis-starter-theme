d2090bfb56360e788f06c4ead675d3d5c944b60a
David Jensen <dkjensen@users.noreply.github.com>
2021-09-07 09:14:32 -0400

    Update .releaserc.js

--- .releaserc.js
+++ .releaserc.js
@@ -1,18 +1,31 @@
 module.exports = {
-    tagFormat: "${version}",
-    branch: "master",
-    plugins: [
-      ["@semantic-release/npm", { npmPublish: false }],
-      "@semantic-release/github",
-      [
-        "semantic-release-plugin-update-version-in-files",
-        {
-          "files": [
-            "package.json",
-            "style.css"
-          ],
-          "placeholder": "0.0.0-development"
-        }
-      ]
+  tagFormat: "${version}",
+  branch: "master",
+  plugins: [
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "changelog.txt"
+      }
+    ],
+    [
+      "@semantic-release/git",
+      {
+        "assets": ["changelog.txt"]
+      }
+    ],
+    ["@semantic-release/npm", { npmPublish: false }],
+    "@semantic-release/github",
+    [
+      "semantic-release-plugin-update-version-in-files",
+      {
+        "files": [
+          "package.json",
+          "style.css"
+        ],
+        "placeholder": "0.0.0-development"
+      }
     ]
-  };
+  ]
+};
